82263dc3c9e71c5dad18e6ace563e2a37cc6a38d
Leo Basevich <lbasevich86@gmail.com>
2024-10-15 16:00:24 +0000

    Complete

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -17,9 +17,8 @@ public class JavalinSingleton {
         Javalin app = Javalin.create();
 
         app.get("/firstname/{first}", ctx -> {
-            
-            //write code here
-
+            String firstname = ctx.pathParam("first");
+            ctx.result(firstname);
         });
 
         return app;
